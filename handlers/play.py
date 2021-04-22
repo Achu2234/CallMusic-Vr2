43c28c50f833128feccf2ce93202948398cf2d24
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-04-22 15:17:19 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -19,7 +19,7 @@ from helpers.decorators import errors
 async def play(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
 
-    res = await message.reply_text("🔄 Processing...")
+    res = await message.reply_text("🔄 Processing...If yo want to chat join ; @Yeageristbots")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
@@ -56,7 +56,7 @@ async def play(_, message: Message):
                         break
 
         if offset in (None,):
-            await res.edit_text("❗️ You did not give me anything to play.")
+            await res.edit_text("❗️ You did not give me anything to play If Any problem join @Animemusicarchive6 And @Yeageristbots .")
             return
 
         url = text[offset:offset + length]
@@ -66,5 +66,5 @@ async def play(_, message: Message):
         position = await queues.put(message.chat.id, file=file)
         await res.edit_text(f"*️⃣ Queued at position {position}")
     else:
-        await res.edit_text("🎧 Playing...")
+        await res.edit_text("🎧 Playing... If Any lags say in @Yeageristbots ")
         await callsmusic.set_stream(message.chat.id, file)
