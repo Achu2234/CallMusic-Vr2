bf23eefdc59cf91cd06493d4e8ed3ceb9c92138b
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-04-22 15:14:25 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+LOG_CHAT = getenv("LOG_CHAT")
+
+# NOTE Don't make changes below this line
+ARQ_API_BASE_URL = "http://35.240.133.234:8000"
