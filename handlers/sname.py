98324047871fcf656d383ab7eaeeea6ba19c60ed
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-04-22 15:19:33 +0530

    Update sname.py

--- handlers/sname.py
+++ handlers/sname.py
@@ -26,7 +26,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('Please wait! Im Finding the song... **Join @NexaBotsUpdates** ❤️')
+    m = message.reply('Please wait! Im Finding the song... **Join @Animemusicarchive6** ❤️')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -55,7 +55,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👀️ **Views**: `{views}`\n **Up By**: **@MusicsNexa_bot** \n\nJoin **@NexaBotsUpdates** ❤️'
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👀️ **Views**: `{views}`\n **Up By**: **@YeageristMusic_bot** \n\nJoin **@Animemusicarchive6** ❤️'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
